37ea47b8b26387142249023ba9fecf54563407b8
Mohammod Ibrahim <143773181+MohammoD2@users.noreply.github.com>
2024-09-07 10:58:08 +0600

    Update Weather_App.py

--- Weather_App.py
+++ Weather_App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import json
-st.title("☁️Weather App☁️")
+
 
 def fetch_weather(city):
     """Fetch weather data from the API."""
@@ -10,12 +10,19 @@ def fetch_weather(city):
     return json.loads(response.text)
 
 def main():
+    st.set_page_config(
+        page_title="☁️Weather Report App☁️",
+        page_icon="☁️",
+        layout="centered",
+        initial_sidebar_state="expanded"
+    )
     st.title("Weather Report App")
 
     # Input field for city
     city = st.text_input("Enter your city name:")
 
     if city:
+        
         # Fetch weather data
         weather_data = fetch_weather(city)
         
